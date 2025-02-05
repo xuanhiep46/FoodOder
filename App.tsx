508cf5cee1ad643fe781fdce213078150b8e78bd
xuanhiep46 <xuanhiep0881888@gmail.com>
2025-02-05 17:23:22 +0700

    video StageAndDataType

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  //typeScript
+  //State---
+  //data-type
+  const[name, setName] = useState<string>("xuanhiep"); //string --- dùng nhiều nhất
+
+  const[age, setAge] = useState<number>(0); //number
+
+  //null, undefined, boolean
+  const test = false;
+
+  //object---
+  const[person, setPerson] = useState<({ //Kh ép kiểu vào, typeScript sẽ tự đoán luôn
+    name: "xuanhiep",
+    age: 20
+  })>({
+    name: "xuanhiep",
+    age: 20
+  }); 
+
+  //array---
+  const[person1, setPerson1] = useState([6, 9]); 
+
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}> {name} </Text>
+        <Text style={styles.text}> {age} </Text>
+        <Text style={styles.text}> {person.age} </Text>
+        <Text style={styles.text}> {JSON.stringify(person1)} </Text> /* nên convert nó qua dạng string */
+        <Text style={styles.text}> {JSON.stringify(person)} </Text> /*chuyển sang dang string*/ 
+      </View>
     </View>
   );
 }
@@ -14,7 +43,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingHorizontal: 20,
   },
+  text: {}
 });
